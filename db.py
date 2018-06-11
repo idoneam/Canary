3e9e3d6d254b77a529094b93e0c96117157b78b0
trungams <vttrung12@gmail.com>
2018-06-11 18:41:08 -0400

    Add welcome/goodbye message

--- db.py
+++ db.py
@@ -229,6 +229,27 @@ class Db():
                                          tablefmt="fancy_grid") +
                                 '```', delete_after=30)
 
+    @asyncio.coroutine
+    def on_member_join(self, member):
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("SELECT * FROM Welcome")
+        greetings = c.fetchall()
+        msg = random.choice(greetings)[0]
+        msg = msg.replace('$user$', member.mention)
+        general = self.bot.get_channel(236668784948019202)
+        yield from general.send(msg)
+
+    @asyncio.coroutine
+    def on_member_leave(self, member):
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute("SELECT * FROM Bye")
+        farewell = c.fetchall()
+        msg = random.choice(farewell)[0]
+        msg = msg.replace('$user$', member.mention)
+        general = self.bot.get_channel(236668784948019202)
+        yield from general.send(msg)
 
 def setup(bot):
     bot.add_cog(Db(bot))
