2ebf86db2e4b543562c7769bf9c12e57a5fc917e
Imad Dodin <imad.dodin@mail.mcgill.ca>
2018-06-12 23:52:02 -0400

    Implement initial functionality for reminders

--- db.py
+++ db.py
@@ -8,6 +8,7 @@ import asyncio
 # For DB Functionality
 import sqlite3
 from tabulate import tabulate
+import datetime
 
 # Other utilities
 import random
@@ -20,6 +21,83 @@ class Db():
     def __init__(self, bot):
         self.bot = bot
 
+    @asyncio.coroutine
+    def check_reminders(self):
+        yield from self.bot.wait_until_ready()
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        g = (guild for guild in self.bot.guilds if guild.name == 'McGill University')
+        guild = next(g)
+        while not self.bot.is_closed():
+            try:
+                reminders = c.execute('SELECT * FROM Reminders').fetchall()
+            except sqlite3.OperationalError:
+                c.execute("CREATE TABLE 'Reminders' ('ID'INTEGER,'Name'TEXT,'Reminder'TEXT,'Date'TEXT,"
+                          "'LastReminder'TEXT)")
+            conn.close()
+            for i in range(len(reminders)):
+                member = discord.utils.get(guild.members, id=reminders[i][0])
+                yield from member.send("Reminding you to {}! [{:d}]".format(reminders[i][2], i + 1))
+                yield from asyncio.sleep(1)
+            yield from asyncio.sleep(60 * 10)
+
+    @commands.command(pass_context=True)
+    @asyncio.coroutine
+    def stop_reminder(self, ctx, reminder: str):
+        """
+        [DM Only] Delete the specified reminder
+        :param reminder: An integer choice for reminder based on Martlet's last set of DM's with reminders.
+        """
+        if isinstance(ctx.message.channel, discord.DMChannel):
+            conn = sqlite3.connect(DB_PATH)
+            c = conn.cursor()
+            try:
+                reminders = c.execute('SELECT * FROM Reminders WHERE ID = ?', (ctx.message.author.id,)).fetchall()
+            except sqlite3.OperationalError:
+                c.execute("CREATE TABLE 'Reminders' ('ID'INTEGER,'Name'TEXT,'Reminder'TEXT,'Date'TEXT,"
+                          "'LastReminder'TEXT)")
+                yield from ctx.send("Database created.")
+                return
+            try:
+                choice = int(reminder)
+                if choice < 1 or choice > len(reminders):
+                    raise ValueError
+            except ValueError:
+                yield from ctx.send("Please specify a choice number between 1 and {:d}!"
+                                    .format(len(reminders)))
+                conn.close()
+                return
+            t = (reminders[choice - 1][2], ctx.message.author.id, reminders[choice - 1][3])
+            c.execute('DELETE FROM Reminders WHERE Reminder=? AND ID=? AND DATE=?', t)
+            conn.commit()
+            conn.close()
+            yield from ctx.send("Reminder successfully removed!")
+        else:
+            yield from ctx.send("Slide into my DM's ;) (Please respond to my DM messages to stop "
+                                "reminders!)")
+
+    @commands.command(pass_context=True)
+    @asyncio.coroutine
+    def remindme(self, ctx, *, quote: str):
+        """
+        Add a reminder to the reminder database.
+        """
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        t = (ctx.message.author.id, ctx.message.author.name, quote, datetime.datetime.now(), datetime.datetime.now())
+        reminders = c.execute('SELECT * FROM Reminders WHERE ID =?', (ctx.message.author.id,)).fetchall()
+        try:
+            c.execute('INSERT INTO Reminders VALUES (?, ?, ?, ?, ?)', t)
+        except sqlite3.OperationalError:
+            c.execute("CREATE TABLE 'Reminders' ('ID'INTEGER,'Name'TEXT,'Reminder'TEXT,'Date'TEXT,'LastReminder'TEXT)")
+            c.execute('INSERT INTO Reminders VALUES (?, ?, ?, ?, ?)', t)
+        yield from ctx.author.send('Hi {}! \n I will remind you to {} daily until you send me a message to stop '
+                                   'reminding you about it! [{:d}]'
+                                   .format(ctx.author.name, quote, len(reminders)+1))
+        yield from ctx.send('Reminder added.')
+        conn.commit()
+        conn.close()
+
     @commands.command(pass_context=True)
     @asyncio.coroutine
     def addq(self, ctx, member: discord.Member, *, quote: str):
@@ -29,7 +107,7 @@ class Db():
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
         t = (member.id, member.name, quote,
-             ctx.message.created_at.strftime("%c"))
+             datetime.date.today())
         c.execute('INSERT INTO Quotes VALUES (?,?,?,?)', t)
         yield from ctx.send('`Quote added.`')
         conn.commit()
@@ -37,13 +115,13 @@ class Db():
 
     @commands.command(pass_context=True)
     @asyncio.coroutine
-    def q(self, ctx, str1: str=None, *, str2: str=None):
+    def q(self, ctx, str1: str = None, *, str2: str = None):
         """
         Retrieve a quote with a specified keyword / mention.
         """
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
-        if str1 is None:    # no argument
+        if str1 is None:  # no argument
             quotes = c.execute('SELECT Quote FROM Quotes').fetchall()
             quote = random.choice(quotes)
             Name = c.execute('SELECT Name FROM Quotes WHERE Quote LIKE ?',
@@ -61,33 +139,33 @@ class Db():
         elif str2 is None:  # 1 argument
             numArgs = 1
             args = str1
-        else:   # 2 arguments
+        else:  # 2 arguments
             numArgs = 2
             argl = [str1, str2]
             args = ' '.join(argl)
-        if (args[1] == '@'):    # member argument supplied
+        if (args[1] == '@'):  # member argument supplied
             args = args.split()
-            if numArgs == 2:    # has query
-                t = ((args[0][3:(len(args[0])-1)]),
-                     '%'+(' '.join(args[1:]))+'%')
+            if numArgs == 2:  # has query
+                t = ((args[0][3:(len(args[0]) - 1)]),
+                     '%' + (' '.join(args[1:])) + '%')
             qId = ''
             for i in range(len(args[0])):
                 if (args[0][i] in '0123456789'):
                     qId = qId + args[0][i]
-            if numArgs == 2:    # query
-                t = (qId, '%'+(' '.join(args[1:]))+'%')
+            if numArgs == 2:  # query
+                t = (qId, '%' + (' '.join(args[1:])) + '%')
                 quoteslist = c.execute(
                     'SELECT Quote FROM Quotes WHERE ID=? AND Quote LIKE ?',
                     t).fetchall()
-            else:   # no query
+            else:  # no query
                 t = (qId,)
                 quoteslist = c.execute(
-                    'SELECT Quote FROM Quotes WHERE ID=?',t).fetchall()
+                    'SELECT Quote FROM Quotes WHERE ID=?', t).fetchall()
             if not quoteslist:  # no result
                 yield from ctx.send('No quotes found.')
                 conn.close()
                 return
-            else:   # result
+            else:  # result
                 quote = random.choice(quoteslist)
                 quote_stripped = quote[0].replace('"', '')
                 if (len(quote_stripped) > 500):
@@ -97,8 +175,8 @@ class Db():
 
                 conn.close()
                 return
-        else:   # no member argument - only query
-            t = ('%'+args[0:]+'%',)
+        else:  # no member argument - only query
+            t = ('%' + args[0:] + '%',)
             quoteslist = c.execute(
                 'SELECT Quote FROM Quotes WHERE Quote LIKE ?', t).fetchall()
             if not quoteslist:
@@ -123,7 +201,7 @@ class Db():
 
     @commands.command(pass_context=True)
     @asyncio.coroutine
-    def lq(self, ctx, str1: str=None):
+    def lq(self, ctx, str1: str = None):
         """
         List your quotes or the quotes of a mentioned user.
         """
@@ -131,7 +209,7 @@ class Db():
             member = ctx.message.author
             t = (member.id,)
         else:
-            t = ((str1[3:(len(str1[0])-2)]),)
+            t = ((str1[3:(len(str1[0]) - 2)]),)
             print(t)
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
@@ -140,12 +218,12 @@ class Db():
         msg = "```Quotes: \n"
         for i in range(len(quoteslist)):
             if ((len(msg) + len('[%d] %s\n' %
-                                (i+1, quoteslist[i][0]))) > 1996):
+                                (i + 1, quoteslist[i][0]))) > 1996):
                 msg += '```'
                 yield from ctx.send(msg, delete_after=30)
-                msg = '```[%d] %s\n' % (i+1, quoteslist[i][0])
+                msg = '```[%d] %s\n' % (i + 1, quoteslist[i][0])
             else:
-                msg += '[%d] %s\n' % (i+1, quoteslist[i][0])
+                msg += '[%d] %s\n' % (i + 1, quoteslist[i][0])
         if ((len(msg) + len('\n ~ End of Quotes ~```')) < 1996):
             msg += '\n ~ End of Quotes ~```'
             yield from ctx.send(msg, delete_after=30)
@@ -175,12 +253,12 @@ class Db():
             msg = "Please choose a quote you would like to delete.\n\n```"
             for i in range(len(quoteslist)):
                 if ((len(msg) + len('[%d] %s\n' %
-                                    (i+1, quoteslist[i][0]))) > 1996):
+                                    (i + 1, quoteslist[i][0]))) > 1996):
                     msg += '```'
                     yield from ctx.send(msg, delete_after=30)
-                    msg = '```[%d] %s\n' % (i+1, quoteslist[i][0])
+                    msg = '```[%d] %s\n' % (i + 1, quoteslist[i][0])
                 else:
-                    msg += '[%d] %s\n' % (i+1, quoteslist[i][0])
+                    msg += '[%d] %s\n' % (i + 1, quoteslist[i][0])
             if ((len(msg) +
                  len('\n[0] Exit without deleting quotes```')) < 1996):
                 msg += '\n[0] Exit without deleting quotes```'
@@ -205,7 +283,7 @@ class Db():
             conn.close()
             return
         else:
-            t = (quoteslist[choice-1][0], ctx.message.author.id)
+            t = (quoteslist[choice - 1][0], ctx.message.author.id)
             c.execute('DELETE FROM Quotes WHERE Quote=? AND ID=?', t)
             yield from ctx.send("Quote successfully deleted.")
             conn.commit()
@@ -225,10 +303,12 @@ class Db():
         for (ID, DisplayName, Upmartlet) in members:
             table.append((DisplayName, Upmartlet))
         yield from ctx.send('```Java\n' +
-                                tabulate(table, headers=["NAME", "#"],
-                                         tablefmt="fancy_grid") +
-                                '```', delete_after=30)
+                            tabulate(table, headers=["NAME", "#"],
+                                     tablefmt="fancy_grid") +
+                            '```', delete_after=30)
 
 
 def setup(bot):
-    bot.add_cog(Db(bot))
+    database = Db(bot)
+    bot.add_cog(database)
+    bot.loop.create_task(database.check_reminders())
