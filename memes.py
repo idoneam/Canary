dfca7fe1d025faa1477b85b01b8eeb3358489510
Imad Dodin <imad.dodin@mail.mcgill.ca>
2018-06-14 22:12:47 -0400

    Use New 3.5+ Asyncio Syntax

--- memes.py
+++ memes.py
@@ -13,21 +13,19 @@ class Memes():
         self.bot = bot
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def lenny(self, ctx):
+    async def lenny(self, ctx):
         """
         Lenny face
         """
-        yield from ctx.send("( ͡° ͜ʖ ͡°) ")
-        yield from ctx.message.delete()
+        await ctx.send("( ͡° ͜ʖ ͡°) ")
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def license(self, ctx):
+    async def license(self, ctx):
         """
         License
         """
-        yield from ctx.send("This bot is free software: you can redistribute"
+        await ctx.send("This bot is free software: you can redistribute"
                             " it and/or modify it under the terms of the GNU"
                             " General Public License as published by the "
                             "Free Software Foundation, either version 3 of "
@@ -44,87 +42,78 @@ class Memes():
                             "along with this bot. The GNU General Public "
                             "License can also be found at "
                             "<http://www.gnu.org/licenses/>.")
-        yield from ctx.message.delete()
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def gohere(self, ctx):
+    async def gohere(self, ctx):
         """
         for future mcgillians
         """
-        yield from ctx.send("http://gph.is/1cN9wO1")
-        yield from ctx.message.delete()
+        await ctx.send("http://gph.is/1cN9wO1")
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def tunak(self, ctx):
+    async def tunak(self, ctx):
         """
         bitch pls
         """
-        yield from ctx.send("http://i.imgur.com/rNNLyjK.gif")
-        yield from ctx.message.delete()
+        await ctx.send("http://i.imgur.com/rNNLyjK.gif")
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def bb8(self, ctx):
+    async def bb8(self, ctx):
         """
         nice job bb8
         """
-        yield from ctx.send("http://i.imgur.com/SUvaUM2.gif")
-        yield from ctx.message.delete()
+        await ctx.send("http://i.imgur.com/SUvaUM2.gif")
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def longtime(self, ctx):
+    async def longtime(self, ctx):
         """
         That's a name I've not heard in a long time
         """
-        yield from ctx.send("http://i.imgur.com/e1T1xcq.mp4")
-        yield from ctx.message.delete()
+        await ctx.send("http://i.imgur.com/e1T1xcq.mp4")
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def thonk(self, ctx):
+    async def thonk(self, ctx):
         """
         when thonking consumes you
         """
-        yield from ctx.send("https://i.imgur.com/VADGUwj.gifv")
-        yield from ctx.message.delete()
+        await ctx.send("https://i.imgur.com/VADGUwj.gifv")
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def dealwithit(self, ctx):
+    async def dealwithit(self, ctx):
         """
         deal with it trump
         """
-        yield from ctx.send("http://i.imgur.com/5jzN8zV.mp4")
-        yield from ctx.message.delete()
+        await ctx.send("http://i.imgur.com/5jzN8zV.mp4")
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def lmao(self, ctx):
+    async def lmao(self, ctx):
         """
         that's hilarious
         """
-        yield from ctx.send("http://i.imgur.com/o5Cc3i2.mp4")
-        yield from ctx.message.delete()
+        await ctx.send("http://i.imgur.com/o5Cc3i2.mp4")
+        await ctx.message.delete()
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def chirp(self, ctx):
+    async def chirp(self, ctx):
         """:^)"""
-        yield from ctx.send('CHIRP CHIRP')
+        await ctx.send('CHIRP CHIRP')
 
     @commands.command(pass_context=True)
-    @asyncio.coroutine
-    def mix(self, ctx, *, inputStr: str=None):
+    async def mix(self, ctx, *, inputStr: str=None):
         if inputStr is None:
-            yield from ctx.send()
+            await ctx.send()
         msg = "".join([(c.upper() if
                         random.randint(0, 1) else
                         c.lower()) for c in inputStr])
-        yield from ctx.send(msg)
-        yield from ctx.message.delete()
+        await ctx.send(msg)
+        await ctx.message.delete()
 
 
 def setup(bot):
--- mod.py
+++ mod.py
@@ -9,30 +9,28 @@ class Mod():
     def __init__(self, bot):
         self.bot = bot
 
-    @asyncio.coroutine
-    def on_message(self, message):
+    async def on_message(self, message):
         if message.author == self.bot.user:
             return
         if isinstance(message.channel, discord.DMChannel):
             channelToSend = self.bot.get_channel(326524201987211267)
             msg = '{} 📣 {}'.format(str(message.author), message.content)
-            yield from channelToSend.send(msg)
+            await channelToSend.send(msg)
 
     @commands.command(pass_context=True)
     @commands.has_role('Discord Moderator')
-    @asyncio.coroutine
-    def pm(self, ctx, user: discord.User, *, message):
+    async def pm(self, ctx, user: discord.User, *, message):
         '''
         PM a user on the server using marty
         '''
         dest = user
-        yield from dest.send(message)
+        await dest.send(message)
 
     # @commands.command(pass_context=True, aliases=['cid'])
     # @commands.has_role('Discord Moderator')
     # @asyncio.coroutine
     # def get_channel_id(self, ctx):
-    #     yield from ctx.send(ctx.channel.id)
+    #     await ctx.send(ctx.channel.id)
 
 
 def setup(bot):
