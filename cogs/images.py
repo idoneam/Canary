8e5330a6d957fac753defa9c090a8ade6ab06a36
trungams <vttrung12@gmail.com>
2018-10-13 13:16:25 -0400

    Format with yapf

--- cogs/images.py
+++ cogs/images.py
@@ -12,6 +12,7 @@ import cv2
 import math
 from functools import wraps
 
+
 def filterImage(func):
     @wraps(func)
     async def wrapper(self, ctx, *args):
@@ -29,7 +30,7 @@ def filterImage(func):
             if len(args) == 2:
                 args = (args[0], result)
             else:
-                args = (result,)
+                args = (result, )
             result = await func(self, ctx, *args)
             cv2.imwrite(dst, result, [cv2.IMWRITE_JPEG_QUALITY, 100])
             await ctx.message.delete()
@@ -38,6 +39,7 @@ def filterImage(func):
         except Exception:
             await ctx.send('Error occurred.', delete_after=60)
         os.remove(src)
+
     return wrapper
 
 
@@ -57,34 +59,26 @@ class Images():
                 return msg.attachments[0]
         return None
 
-
     def _polar(self, image):
         height, width = image.shape[:2]
-        centerX = width/2
-        centerY = height/2
-        radius = math.sqrt(width**2 + height**2)/2
+        centerX = width / 2
+        centerY = height / 2
+        radius = math.sqrt(width**2 + height**2) / 2
 
-        result = cv2.linearPolar(image,
-            (centerX, centerY),
-            radius,
-            cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS
-        )
+        result = cv2.linearPolar(image, (centerX, centerY), radius,
+                                 cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS)
         return result
-
 
     def _cart(self, image):
         height, width = image.shape[:2]
-        centerX = width/2
-        centerY = height/2
-        radius = math.sqrt(width**2 + height**2)/2
+        centerX = width / 2
+        centerY = height / 2
+        radius = math.sqrt(width**2 + height**2) / 2
 
-        result = cv2.linearPolar(image,
-            (centerX, centerY),
-            radius,
-            cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS + cv2.WARP_INVERSE_MAP
-        )
+        result = cv2.linearPolar(
+            image, (centerX, centerY), radius,
+            cv2.INTER_LINEAR + cv2.WARP_FILL_OUTLIERS + cv2.WARP_INVERSE_MAP)
         return result
-
 
     @commands.command()
     @filterImage
@@ -95,7 +89,6 @@ class Images():
         image = self._polar(image)
         return np.rot90(image, -1)
 
-
     @commands.command()
     @filterImage
     async def cart(self, ctx, image=None):
@@ -105,7 +98,6 @@ class Images():
         image = self._cart(np.rot90(image))
         return image
 
-
     @commands.command()
     @filterImage
     async def blur(self, ctx, iterations='1', image=None):
@@ -114,9 +106,8 @@ class Images():
         '''
         iterations = max(0, min(int(iterations), 100))
         for i in range(iterations):
-            image = cv2.GaussianBlur(image, (5,5), 0)
+            image = cv2.GaussianBlur(image, (5, 5), 0)
         return image
-
 
     @commands.command(aliases=['left', 'right'])
     @filterImage
@@ -125,20 +116,18 @@ class Images():
         Blur the image horizontally
         '''
         radius = max(1, min(int(radius), 500))
-        image = cv2.blur(image, (radius,1))
+        image = cv2.blur(image, (radius, 1))
         return image
-
 
     @commands.command(aliases=['up', 'down'])
     @filterImage
-    async def vblur(self, ctx,radius='10', image=None):
+    async def vblur(self, ctx, radius='10', image=None):
         '''
         Blur the image vertically
         '''
         radius = max(1, min(int(radius), 500))
-        image = cv2.blur(image, (1,radius))
+        image = cv2.blur(image, (1, radius))
         return image
-
 
     @commands.command(aliases=['zoom', 'radial'])
     @filterImage
@@ -148,10 +137,9 @@ class Images():
         '''
         radius = max(1, min(int(radius), 500))
         image = self._polar(image)
-        image = cv2.blur(image, (radius,1))
+        image = cv2.blur(image, (radius, 1))
         image = self._cart(image)
         return image
-
 
     @commands.command(aliases=['circle', 'circular', 'spin'])
     @filterImage
@@ -163,20 +151,22 @@ class Images():
         halfRadius = radius // 2
         # determine values for padding
         height, width = image.shape[:2]
-        r = math.sqrt(width**2 + height**2)//2
+        r = math.sqrt(width**2 + height**2) // 2
         verticalPad = int(r - height / 2)
         horizontalPad = int(r - width / 2)
         # pad border to avoid black regions when transforming image back to normal
-        image = cv2.copyMakeBorder(image, verticalPad, verticalPad, horizontalPad, horizontalPad, cv2.BORDER_REPLICATE)
+        image = cv2.copyMakeBorder(image, verticalPad, verticalPad,
+                                   horizontalPad, horizontalPad,
+                                   cv2.BORDER_REPLICATE)
         image = self._polar(image)
         # wrap border to avoid the sharp horizontal line when transforming image back to normal
-        image = cv2.copyMakeBorder(image, halfRadius, halfRadius, halfRadius, halfRadius, cv2.BORDER_WRAP)
-        image = cv2.blur(image, (1,radius))
+        image = cv2.copyMakeBorder(image, halfRadius, halfRadius, halfRadius,
+                                   halfRadius, cv2.BORDER_WRAP)
+        image = cv2.blur(image, (1, radius))
         image = image[halfRadius:-halfRadius, halfRadius:-halfRadius]
         image = self._cart(image)
         image = image[verticalPad:-verticalPad, horizontalPad:-horizontalPad]
         return image
-
 
     @commands.command(aliases=['df', 'dfry', 'fry'])
     @filterImage
@@ -190,15 +180,14 @@ class Images():
         for i in range(iterations):
             std = int(np.std(image))
             # Contrast
-            image = cv2.addWeighted(image, 0.9, image, 0, std*0.3)
+            image = cv2.addWeighted(image, 0.9, image, 0, std * 0.3)
             # Sharpness
             image = cv2.filter2D(image, 0, kernel)
             # Saturation
             image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            image[:,:,1:] = cv2.add(image[:,:,1:], image[:,:,1:])
+            image[:, :, 1:] = cv2.add(image[:, :, 1:], image[:, :, 1:])
             image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
         return image
-
 
     @commands.command()
     @filterImage
@@ -211,8 +200,9 @@ class Images():
         for i in range(iterations):
             noise = np.std(image) * np.random.random(image.shape)
             image = cv2.add(image, noise.astype('uint8'))
-            image = cv2.addWeighted(image, 1, image, 0, -np.std(image)*0.49)
+            image = cv2.addWeighted(image, 1, image, 0, -np.std(image) * 0.49)
         return image
+
 
 def setup(bot):
     bot.add_cog(Images(bot))
