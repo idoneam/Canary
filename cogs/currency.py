d83caaae43f2b0e01a0b20cfbd6de723886b30fe
David Lougheed <david.lougheed@gmail.com>
2019-01-13 19:00:00 -0500

    Add a bunch of new currency stuff, with decimal support and helper functions.

--- cogs/currency.py
+++ cogs/currency.py
@@ -6,11 +6,14 @@ from discord.ext import commands
 import asyncio
 
 # For type hinting
-from typing import Dict
+from typing import Dict, Optional
 
 # For DB functionality
 import sqlite3
 import datetime
+
+# For general currency shenanigans
+from decimal import *
 
 # For betting
 import random
@@ -18,40 +21,46 @@ import random
 # For other stuff
 import json
 
-CLAIM_AMOUNT = 20
+CLAIM_AMOUNT = Decimal(20)
 CLAIM_WAIT_TIME = datetime.timedelta(hours=1)
 
 COIN_FLIP_CHOICES = ("h", "t")
 
+CURRENCY_ALL = ["all", "tout"]
+
+ACTION_INITIAL_CLAIM = "init_claim"
 ACTION_CLAIM = "claim"
-ACTION_COIN_FLIP = "coin_flip"
+ACTION_BET_FLIP = "bet_flip"
+ACTION_BET_ROLL = "bet_roll"
 ACTION_GIFTER = "gifter"
 ACTION_GIFTEE = "giftee"
 
-TRANSACTION_ACTIONS = (ACTION_CLAIM, ACTION_COIN_FLIP, ACTION_GIFTER,
-                       ACTION_GIFTEE)
+TRANSACTION_ACTIONS = (ACTION_INITIAL_CLAIM, ACTION_CLAIM, ACTION_BET_FLIP,
+                       ACTION_BET_ROLL, ACTION_GIFTER, ACTION_GIFTEE)
 
 
 class Currency:
     def __init__(self, bot):
         self.bot = bot
+        self.currency = self.bot.config.currency
+        self.prec = self.currency["precision"]
 
-    async def fetch_bank_balance(self, user: discord.Member):
+    async def fetch_bank_balance(self, user: discord.Member) -> Decimal:
         conn = sqlite3.connect(self.bot.config.db_path)
         c = conn.cursor()
-        c.execute("SELECT SUM(Amount) FROM BankTransactions WHERE UserID = ?",
-                  (user.id, ))
+        c.execute("SELECT IFNULL(SUM(Amount), 0) FROM BankTransactions WHERE "
+                  "UserID = ?", (user.id, ))
 
-        balance = c.fetchone()[0]
+        balance = self.db_to_currency(c.fetchone()[0])
         if balance is None:
-            balance = 0
+            balance = Decimal(0)
 
         conn.close()
 
         return balance
 
     async def create_bank_transaction(self, c, user: discord.Member,
-                                      amount: int, action: str,
+                                      amount: Decimal, action: str,
                                       metadata: Dict):
         # Don't create another connection in this function in order to properly
         # transaction-ify a series of bank "transactions".
@@ -61,20 +70,97 @@ class Currency:
             return
 
         now = int(datetime.datetime.now().timestamp())
-        t = (user.id, amount, action, json.dumps(metadata), now)
+        t = (user.id, self.currency_to_db(amount), action,
+             json.dumps(metadata), now)
         c.execute(
             "INSERT INTO BankTransactions(UserID, Amount, Action, "
             "Metadata, Date) VALUES(?, ?, ?, ?, ?)", t)
 
-    def parse_currency(self, amount: str, balance: int):
-        if amount.lower().strip() == "all":
-            amount_int = balance
+    def parse_currency(self, amount: str, balance: Decimal):
+        if amount.lower().strip() in CURRENCY_ALL:
+            return balance
         else:
             try:
-                amount_int = int(amount)
-            except ValueError:
+                return Decimal(amount)
+            except InvalidOperation:
+                # Value error (invalid conversion)
                 return None
-        return amount_int
+
+    def currency_to_db(self, amount: Decimal):
+        return int(amount * Decimal(10 ** self.currency["precision"]))
+
+    def db_to_currency(self, amount: int):
+        return Decimal(amount) / Decimal(10 ** self.currency["precision"])
+
+    def format_currency(self, amount: Decimal):
+        return ("{:." + str(self.prec) + "f}").format(amount)
+
+    def format_symbol_currency(self, amount: Decimal):
+        return self.currency["symbol"] + self.format_currency(amount)
+
+    def check_bet(self, balance: Decimal, bet: Decimal) -> Optional[str]:
+        """
+        Checks universally invalid betting cases.
+        """
+
+        # - Balance-related subcase
+
+        if balance <= 0:
+            return "You're too broke to bet!"
+
+        # - Bet-related subcases
+
+        if bet is None:
+            # Invalid conversion to decimal.
+            return "Invalid betting quantity: '{}'.".format(bet)
+
+        if bet <= 0:
+            return "Please bet a positive amount."
+
+        if bet > balance:
+            return "You're too broke to bet that much!"
+
+        return ""
+
+    @commands.command()
+    async def initial_claim(self, ctx):
+        """
+        Claim's the user's start-up grant currency.
+        """
+
+        # Start bot typing
+        await ctx.trigger_typing()
+
+        conn = sqlite3.connect(self.bot.config.db_path)
+        c = conn.cursor()
+
+        c.execute(
+            "SELECT IFNULL(MAX(Date), 0) FROM BankTransactions "
+            "WHERE UserID = ? AND Action = ?",
+            (ctx.message.author.id, ACTION_INITIAL_CLAIM))
+
+        claim_time = c.fetchone()[0]
+
+        author_name = ctx.message.author.display_name
+
+        if claim_time > 0:
+            await ctx.send("{} has already claimed their initial "
+                           "currency.".format(author_name))
+            return
+
+        metadata = {"channel": ctx.message.channel.id}
+
+        await self.create_bank_transaction(
+            c, ctx.message.author, self.currency["initial_amount"],
+            ACTION_INITIAL_CLAIM, metadata)
+
+        conn.commit()
+
+        await ctx.send("{} claimed their initial {}!".format(
+            author_name,
+            self.format_symbol_currency(self.currency["initial_amount"])))
+
+        conn.close()
 
     @commands.command()
     async def claim(self, ctx):
@@ -103,11 +189,13 @@ class Currency:
             metadata = {"channel": ctx.message.channel.id}
 
             await self.create_bank_transaction(
-                c, ctx.message.author, CLAIM_AMOUNT, ACTION_CLAIM, metadata)
+                c, ctx.message.author, decimal.Decimal(CLAIM_AMOUNT),
+                ACTION_CLAIM, metadata)
 
             conn.commit()
 
-            await ctx.send("{} claimed ${}!".format(author_name, CLAIM_AMOUNT))
+            await ctx.send("{} claimed {}!".format(
+                author_name, self.format_symbol_currency(CLAIM_AMOUNT)))
 
         else:
             time_left = last_claimed - threshold
@@ -125,50 +213,40 @@ class Currency:
         # Start bot typing
         await ctx.trigger_typing()
 
-        author = user if user else ctx.message.author
-        author_name = author.display_name if author else ":b:roken bot"
+        # TODO: TEST ACCOUNTS NOT IN THE SERVER?
 
-        await ctx.send("{} has ${} in their account.".format(
-            author_name, await self.fetch_bank_balance(author)))
+        author = user if user else ctx.message.author
+        amount = self.format_symbol_currency(
+            await self.fetch_bank_balance(author))
+
+        await ctx.send("{} has {} in their account.".format(
+            author.display_name, amount))
 
     @commands.command(aliases=["bf"])
-    async def flip(self, ctx, bet: str = None, face: str = None):
+    async def bet_flip(self, ctx, bet: str = None, face: str = None):
         """
         Bets an amount of money on a coin flip.
-        Usage: ?flip h 10 or ?flip t 5
+        Usage: ?bet_flip h 10 or ?bet_flip t 5
         """
 
         if face is None or bet is None:
             return
 
+        # Start bot typing
+        await ctx.trigger_typing()
+
         balance = await self.fetch_bank_balance(ctx.message.author)
-        bet_int = self.parse_currency(bet, balance)
+        bet_dec = self.parse_currency(bet, balance)
         choice = face.strip().lower()
 
         # Handle invalid cases
 
-        # - Balance-related subcase
-
-        if balance <= 0:
-            await ctx.send("You're too broke to bet!")
-            return
-
-        # - Bet-related subcases
-
-        if bet_int is None:
-            await ctx.send("Invalid betting quantity: '{}'.".format(bet))
-            return
-
-        if bet_int <= 0:
-            await ctx.send("Please bet a positive amount.")
-            return
-
-        if bet_int > balance:
-            await ctx.send("You're too broke to bet that much!")
+        error = self.check_bet(balance, bet_dec)
+        if error != "":
+            await ctx.send(error)
             return
 
         # - Choice-related subcase
-
         if choice not in COIN_FLIP_CHOICES:
             await ctx.send("Please choose either h or t.")
             return
@@ -178,23 +256,87 @@ class Currency:
         conn = sqlite3.connect(self.bot.config.db_path)
         c = conn.cursor()
 
-        author_name = (ctx.message.author.display_name
-                       if ctx.message.author else ":b:roken bot")
-
         result = random.choice(COIN_FLIP_CHOICES)
 
         metadata = {"result": result, "channel": ctx.message.channel.id}
 
-        amount = bet_int if choice == result else -bet_int
+        amount = bet_dec if choice == result else -bet_dec
         await self.create_bank_transaction(c, ctx.message.author, amount,
-                                           ACTION_COIN_FLIP, metadata)
+                                           ACTION_BET_FLIP, metadata)
         conn.commit()
 
-        message = "Sorry! {} lost ${} (result was **{}**)."
+        message = "Sorry! {} lost {} (result was **{}**)."
         if choice == result:
-            message = "Congratulations! {} won ${} on **{}**"
+            message = "Congratulations! {} won {} on **{}**"
 
-        await ctx.send(message.format(author_name, bet_int, result))
+        author_name = ctx.message.author.display_name
+
+        await ctx.send(message.format(
+            author_name, self.format_symbol_currency(bet_dec), result))
+
+        conn.close()
+
+    @commands.command(aliases=["br"])
+    async def bet_roll(self, ctx, bet: str = None):
+        """
+        Bets an amount of currency on a D100 roll.
+        Usage: ?bet_roll 100 or ?br all
+        """
+
+        if bet is None:
+            return
+
+        # Start bot typing
+        await ctx.trigger_typing()
+
+        balance = await self.fetch_bank_balance(ctx.message.author)
+        bet_dec = self.parse_currency(bet, balance)
+
+        # Handle invalid cases
+        error = self.check_bet(balance, bet_dec)
+        if error != "":
+            await ctx.send(error)
+            return
+
+        # If all cases pass, perform the gamble
+
+        conn = sqlite3.connect(self.bot.config.db_path)
+        c = conn.cursor()
+
+        result = random.randrange(1, 101)
+        amount_returned = Decimal(0)
+
+        for case, amount in self.currency["bet_roll_cases"]:
+            if result <= case:
+                amount_returned = bet_dec * amount
+                break
+
+        metadata = {
+            "result": result,
+            "bet": str(bet_dec),
+            "returned": self.format_currency(amount_returned),
+            "channel": ctx.message.channel.id
+        }
+
+        await self.create_bank_transaction(c, ctx.message.author, amount_returned - bet_dec,
+                                           ACTION_BET_ROLL, metadata)
+
+        conn.commit()
+
+        message = "Sorry! {un} lost {am} (result was **{re}**)."
+        if amount_returned == bet_dec:
+            message = "{un} broke even (result was **{re}**)."
+        elif amount_returned > bet_dec:
+            message = "Congratulations! {un} won [net] {am} (result was " \
+                      "**{re}**)."
+
+        author_name = ctx.message.author.display_name
+
+        amount_msg_multiplier = -1 if amount_returned < bet_dec else 1
+        bet_str = self.format_symbol_currency(
+            amount_msg_multiplier * (amount_returned - bet_dec))
+
+        await ctx.send(message.format(un=author_name, am=bet_str, re=result))
 
         conn.close()
 
@@ -203,6 +345,9 @@ class Currency:
         """
         Gives some amount of currency to another user.
         """
+
+        # Start bot typing
+        await ctx.trigger_typing()
 
         if not user or amount is None:
             await ctx.send("Usage: ?give [user] [amount]")
@@ -214,19 +359,20 @@ class Currency:
             await ctx.send("You're too broke to give anyone anything!")
             return
 
-        amount_int = self.parse_currency(amount, balance)
+        amount_dec = self.parse_currency(amount, balance)
 
         # Handle invalid cases
 
-        if amount_int is None:
+        if amount_dec is None:
             await ctx.send("Invalid quantity: '{}'.".format(amount))
             return
 
-        if amount_int <= 0:
-            await ctx.send("You cannot give ${}!".format(amount_int))
+        if amount_dec <= 0:
+            await ctx.send("You cannot give {}!".format(
+                self.format_symbol_currency(amount_dec)))
             return
 
-        if amount_int > balance:
+        if amount_dec > balance:
             await ctx.send("You do not have that much money!")
             return
 
@@ -252,15 +398,16 @@ class Currency:
         conn = sqlite3.connect(self.bot.config.db_path)
         c = conn.cursor()
 
-        await self.create_bank_transaction(c, ctx.message.author, -amount_int,
+        await self.create_bank_transaction(c, ctx.message.author, -amount_dec,
                                            ACTION_GIFTER, gifter_metadata)
 
-        await self.create_bank_transaction(c, user, amount_int, ACTION_GIFTEE,
+        await self.create_bank_transaction(c, user, amount_dec, ACTION_GIFTEE,
                                            giftee_metadata)
 
         conn.commit()
 
-        await ctx.send("{} gave ${} to {}!".format(grn, amount_int, gen))
+        await ctx.send("{} gave {} to {}!".format(
+            grn, self.format_symbol_currency(amount_dec), gen))
 
         conn.close()
 
--- config/parser.py
+++ config/parser.py
@@ -1,6 +1,9 @@
 import codecs
 import configparser
 
+# Currency
+import decimal
+
 
 class Parser:
     def __init__(self):
@@ -15,3 +18,34 @@ class Parser:
         self.goodbye = config['Greetings']['Goodbye'].split('\n')
 
         self.db_path = config['DB']['Path']
+
+        # Below lies currency configuration
+
+        currency_precision = int(config["Currency"]["Precision"])
+
+        income_tb = zip([x.strip() for x in config["IncomeTax"]["Brackets"].split(",")],
+                        [x.strip() for x in config["IncomeTax"]["Amounts"].split(",")])
+
+        asset_tb = zip([x.strip() for x in config["AssetTax"]["Brackets"].split(",")],
+                       [x.strip() for x in config["AssetTax"]["Amounts"].split(",")])
+
+        br_cases = zip([x.strip() for x in config["Betting"]["RollCases"].split(",")],
+                       [x.strip() for x in config["Betting"]["RollReturns"].split(",")])
+
+        self.currency = {
+            "name": config["Currency"]["Name"],
+            "symbol": config["Currency"]["Symbol"],
+            "precision": currency_precision,
+            "initial_amount": decimal.Decimal(config["Currency"]["Initial"]),
+            "salary_base": decimal.Decimal(config["Currency"]["SalaryBase"]),
+            "inflation": decimal.Decimal(config["Currency"]["Inflation"]),
+
+            "income_tax": {decimal.Decimal(b): float(a) for b, a in income_tb},
+            "asset_tax": {decimal.Decimal(b): float(a) for b, a in asset_tb},
+
+            "transaction_tax": float(config["OtherTax"]["TransactionTax"]),
+
+            "bet_roll_cases": sorted([(int(c), decimal.Decimal(a))
+                                      for c, a in br_cases],
+                                     key=lambda c: c[0])
+        }
