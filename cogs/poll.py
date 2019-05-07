1b9a87c590ba5dce402444194b3b363921b78360
Mathieu Bérubé-Vallières <47489943+le-potate@users.noreply.github.com>
2019-05-07 01:21:44 -0400

    trying to please yapf

--- cogs/poll.py
+++ cogs/poll.py
@@ -3,12 +3,12 @@ import string
 from discord.ext.commands import Bot
 
 ALPHABET = list(string.ascii_uppercase)
-EMOJI_ALPHABET = [u"\U0001F1E6", u"\U0001F1E7", u"\U0001F1E8", u"\U0001F1E9", 
-                      u"\U0001F1EA", u"\U0001F1EB", u"\U0001F1EC", u"\U0001F1ED", 
-                      u"\U0001F1EE", u"\U0001F1EF", u"\U0001F1F0", u"\U0001F1F1", 
-                      u"\U0001F1F2", u"\U0001F1F3", u"\U0001F1F4", u"\U0001F1F5", 
-                      u"\U0001F1F6", u"\U0001F1F7", u"\U0001F1F8", u"\U0001F1F9"]
-
+EMOJI_ALPHABET = [
+    u"\U0001F1E6", u"\U0001F1E7", u"\U0001F1E8", u"\U0001F1E9", u"\U0001F1EA",
+    u"\U0001F1EB", u"\U0001F1EC", u"\U0001F1ED", u"\U0001F1EE", u"\U0001F1EF",
+    u"\U0001F1F0", u"\U0001F1F1", u"\U0001F1F2", u"\U0001F1F3", u"\U0001F1F4",
+    u"\U0001F1F5", u"\U0001F1F6", u"\U0001F1F7", u"\U0001F1F8", u"\U0001F1F9"
+]
 client = Bot(command_prefix=BOT_PREFIX)
 
 @client.command()
@@ -18,8 +18,13 @@ async def poll(ctx, question, *args):
         return
     username = ctx.message.author.name
     pfp = ctx.message.author.avatar_url
-    embed = discord.Embed(colour=discord.Colour(0x972b67), description="```{}```**To vote, click on one or many emojis**".format(question)) #if single choice is implemented, correct this
-    embed.set_author(name="{} created a poll with {} choices!".format(username,len(args)), icon_url=pfp)
+    embed = discord.Embed(
+      colour=discord.Colour(0x972b67),
+      description="```{}```**To vote, click on one or many emojis**".format(
+            question))    #if single choice is implemented, correct this
+    embed.set_author(
+        name="{} created a poll with {} choices!".format(username, len(args)),
+        icon_url=pfp)
     #eventually add this: embed.set_footer(text="5 users voted • Current winner: D (or top n if specificed) • Options: Non-anonymous, Multiple choice")
 
     pos = 0
