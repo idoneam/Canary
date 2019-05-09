5fb14f515cd254134f8bd74a3d457db021adb089
Trung Vuong Thien <vttrung12@gmail.com>
2019-05-09 13:07:54 -0400

    add license notice to source files

--- cogs/images.py
+++ cogs/images.py
@@ -1,4 +1,21 @@
-#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) idoneam (2016-2019)
+#
+# This file is part of Canary
+#
+# Canary is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canary is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Canary. If not, see <https://www.gnu.org/licenses/>.
 
 # imports for Discord
 import discord
