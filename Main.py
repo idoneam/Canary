c1900e0b97b9e6fd3bf8f3ba91e13378e9733b1f
Mathieu Bérubé-Vallières <47489943+le-potate@users.noreply.github.com>
2019-05-26 00:21:45 -0400

    Overwrite `on_command_error` to make it log too
    I didn't make it stop printing to console because I thought it might still be useful, but it could be removed (line 184 and 186)
    Also changed a print statement for a log (195)

--- Main.py
+++ Main.py
@@ -153,12 +153,46 @@ async def backup(ctx):
     bot.logger.info('Database backup')
 
 
+@bot.event
+async def on_command_error(ctx, error):
+    """The event triggered when an error is raised while invoking a command.
+    ctx   : Context
+    error : Exception"""
+
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    ignored = (commands.CommandNotFound, commands.UserInputError)
+    error = getattr(error, 'original', error)
+
+    if isinstance(error, ignored):
+        return
+
+    elif isinstance(error, commands.DisabledCommand):
+        return await ctx.send(f'{ctx.command} has been disabled.')
+
+    elif isinstance(error, commands.NoPrivateMessage):
+        try:
+            return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+        except:
+            pass
+
+    elif isinstance(error, commands.BadArgument):
+        if ctx.command.qualified_name == 'tag list':
+            return await ctx.send('I could not find that member. Please try again.')
+
+    print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+    bot.logger.info('Ignoring exception in command {}:'.format(ctx.command))
+    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+    bot.logger.info(''.join(traceback.format_exception(type(error), error, error.__traceback__)))
+
+
 if __name__ == "__main__":
     for extension in startup:
         try:
             bot.load_extension(extension)
         except Exception as e:
-            print('Failed to load extension {}\n{}: {}'.format(
+            bot.logger.info('Failed to load extension {}\n{}: {}'.format(
                 extension,
                 type(e).__name__, e))
     bot.run(bot.config.discord_key)
