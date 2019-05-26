4875852e0ccae4084b58a19ed9ca93fbc1a362e2
Mathieu Bérubé-Vallières <47489943+le-potate@users.noreply.github.com>
2019-05-26 00:54:03 -0400

    overwrite discord.py's on_command_error
    (moved from main.py)

--- bot.py
+++ bot.py
@@ -25,6 +25,8 @@ from discord.ext import commands
 from config import parser
 import logging
 import sqlite3
+import sys
+import traceback
 
 _parser = parser.Parser()
 command_prefix = _parser.command_prefix
@@ -58,3 +60,41 @@ class Canary(commands.Bot):
         conn.commit()
         conn.close()
         self.logger.info('Database is ready')
+
+    async def on_command_error(self, ctx, error):
+        """The event triggered when an error is raised while invoking a command.
+        ctx   : Context
+        error : Exception"""
+
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        ignored = (commands.CommandNotFound, commands.UserInputError)
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, ignored):
+            return
+
+        elif isinstance(error, commands.DisabledCommand):
+            return await ctx.send(f'{ctx.command} has been disabled.')
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            try:
+                return await ctx.author.send(
+                    f'{ctx.command} can not be used in Private Messages.')
+            except:
+                pass
+
+        elif isinstance(error, commands.BadArgument):
+            if ctx.command.qualified_name == 'tag list':
+                return await ctx.send(
+                    'I could not find that member. Please try again.')
+
+        print(
+            'Ignoring exception in command {}:'.format(ctx.command),
+            file=sys.stderr)
+        self.logger.info('Ignoring exception in command {}:'.format(ctx.command))
+        traceback.print_exception(
+            type(error), error, error.__traceback__, file=sys.stderr)
+        self.logger.info(''.join(
+            traceback.format_exception(type(error), error, error.__traceback__)))
