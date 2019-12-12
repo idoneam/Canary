c1fdc43a21ef87c41d6fd2ad616b5818bc70766e
David Lougheed <david.lougheed@gmail.com>
2019-12-11 23:07:57 -0500

    Reformat and pythonify role commands

--- cogs/info.py
+++ cogs/info.py
@@ -33,10 +33,9 @@ class Info(commands.Cog):
     @commands.command()
     async def roles(self, ctx):
         """Returns list of all roles in server"""
-        roleNames = list(map(lambda role: role.name + "\n", ctx.guild.roles))
-        roleNames.reverse()
+        role_names = [role.name + "\n" for role in reversed(ctx.guild.roles)]
         p = Pages(ctx,
-                  item_list=roleNames,
+                  item_list=role_names,
                   title="All roles in server",
                   display_option=(3, 20),
                   editable_content=False)
@@ -44,32 +43,34 @@ class Info(commands.Cog):
         await p.paginate()
 
     @commands.command()
-    async def inrole(self, ctx, *, queryRole):
+    async def inrole(self, ctx, *, query_role):
         """Returns list of users in the specified role"""
-        members = None
-        for role in ctx.guild.roles:
-            if role.name.lower() == queryRole.lower():
-                members = role.members
-                break
 
-        if (members is None): return
+        # Get first matching role if one exists
+        try:
+            role = next(role for role in ctx.guild.roles
+                        if role.name.lower() == query_role.lower())
+        except StopIteration:
+            # No matching role exists
+            return
 
-        names = list(map(lambda m: str(m) + "\n", members))
+        names = [str(m) + "\n" for m in role.members]
         header = "List of users in {role} role - {num}".format(role=role.name,
                                                                num=len(names))
 
         # TODO remove for paginator take empty list for embed
-        if (len(names) == 0):
+        if len(names) == 0:
             em = discord.Embed(title=header, colour=0xDA291C)
             em.set_footer(text="Page 01 of 01")
             await ctx.send(embed=em)
-        else:
-            pages = Pages(ctx,
-                          item_list=names,
-                          title=header,
-                          display_option=(3, 20),
-                          editable_content=False)
-            await pages.paginate()
+            return
+
+        pages = Pages(ctx,
+                      item_list=names,
+                      title=header,
+                      display_option=(3, 20),
+                      editable_content=False)
+        await pages.paginate()
 
 
 def setup(bot):
