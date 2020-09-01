0da1b31ae6033d939c6ebd262a9593b8236012c3
Mathieu Bérubé-Vallières <le-potate@users.noreply.github.com>
2020-09-01 13:20:44 -0400

    Rewrite score.py to work with all emojis
    **Why this change was necessary**
    This change was necessary to get better stats, and be able to track reactions better.
    
    **What this change does**
    -This is a near complete rewrite of score.py
    -On raw reaction add/remove (for every emoji), the ReacterID, ReacteeID, ReactionName and MessageID is added/removed to/from the Reactions table
    -If the Reacter or Reactee users aren't in the Members table, they are added. If the user isn't in the server anymore, take a little bit more time to try and find their name in the rest of Discord (using fetch_user instead of get_user). If the account is deleted, use the ID as name.
    -If a user updates their username, update it in the database (this should possibly be moved out of score.py in the future)
    -Rewrite ?score: It now calculates score directly from the Reactions table (the result is still nearly instant). If no argument is given, gives the total score for the user who called it (upmartlets - downmartlets). If a mention is given, gives the total score for the user mentioned. If an emoji is given (but no mention), gives the score for that emoji for the user who called it. If an emoji and a mention is given (*in any order*), gives the score for that emoji for the user mentioned. (For all of these, self-reacts or ignored).
    -Rewrite ?ranking: Also calculates scores directly from the Reactions table (and even tho it must calculate score for many users, the result is still nearly instant!). If no argument is given, the total score ranking is given (Users that never got an upmartlet or downmartlet won't appear there). If an emoji is given, gives the ranking for that emoji. Now uses an embed instead of the fancy_grid.

--- cogs/score.py
+++ cogs/score.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) idoneam (2016-2019)
+# Copyright (C) idoneam (2016-2020)
 #
 # This file is part of Canary
 #
@@ -25,6 +25,7 @@ from discord.ext import commands
 import sqlite3
 from tabulate import tabulate
 from .utils.paginator import Pages
+from collections import Counter
 
 
 class Score(commands.Cog):
@@ -42,146 +43,217 @@ class Score(commands.Cog):
         self.DOWNMARTLET = discord.utils.get(
             self.guild.emojis, name=self.bot.config.downvote_emoji)
 
-    def _get_score(self, emoji):
-        if emoji.id == self.UPMARTLET.id:
-            return 1
+    async def _get_name_from_id(self, user_id):
+        try:
+            user = self.bot.get_user(user_id)
+            if user is None:
+                raise AttributeError
+            name = str(user)
+        except AttributeError:
+            try:
+                user = await self.bot.fetch_user(user_id)
+                name = str(user)
+                if "Deleted User" in username:
+                    name = str(user_id)
+            except discord.errors.NotFound:
+                name = str(user_id)
+        return name
 
-        elif emoji.id == self.DOWNMARTLET.id:
-            return -1
+    async def _add_member_if_needed(self, user_id):
+        conn = sqlite3.connect(self.bot.config.db_path)
+        c = conn.cursor()
+        c.execute("SELECT Name FROM Members WHERE ID = ?", (user_id, ))
+        fetch = c.fetchall()
+        if len(fetch) == 0:
+            name = await self._get_name_from_id(user_id)
+            c.execute('INSERT OR IGNORE INTO Members VALUES (?,?)',
+                      (user_id, name))
+        conn.commit()
+        conn.close()
 
-        return 0
-
-    async def _update_score_if_needed(self, payload, negated=False):
-        # Check for Martlet emoji + upmartletting yourself
-
+    async def _add_or_remove_reaction_from_db(self, payload, remove=False):
         channel = self.bot.get_channel(payload.channel_id)
+        message_id = payload.message_id
 
         try:
-            message = await channel.fetch_message(payload.message_id)
+            message = await channel.fetch_message(message_id)
         except discord.errors.NotFound:
             return
 
-        user = self.bot.get_user(payload.user_id)
+        reacter_id = self.bot.get_user(payload.user_id).id
+        await self._add_member_if_needed(reacter_id)
+        reactee_id = message.author.id
+        await self._add_member_if_needed(reactee_id)
+
         emoji = payload.emoji
-
-        if user == message.author:
-            return
-
-        score = self._get_score(emoji) * (-1 if negated else 1)
-        if score == 0:
-            return
 
         conn = sqlite3.connect(self.bot.config.db_path)
         c = conn.cursor()
-        t = (score, message.author.id)
-
-        if c.execute('SELECT * FROM Members WHERE ID=?', t[1:]).fetchall():
-            # Member record already exists
-            c.execute('UPDATE Members SET Score=Score+? WHERE ID=?', t)
+        if remove:
+            c.execute(
+                'DELETE FROM Reactions WHERE ReacterID = ? AND ReacteeID = ? '
+                'AND ReactionName = ? AND MessageID = ?',
+                (reacter_id, reactee_id, str(emoji), message_id))
         else:
-            # No record exists for the user yet
-            c.execute('INSERT INTO Members VALUES (?, ?, ?)',
-                      (message.author.id, message.author.display_name, score))
-
+            c.execute('INSERT OR IGNORE INTO Reactions VALUES (?,?,?,?)',
+                      (reacter_id, reactee_id, str(emoji), message_id))
         conn.commit()
         conn.close()
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        await self._update_score_if_needed(payload)
+        if payload.guild_id == self.guild.id:
+            await self._add_or_remove_reaction_from_db(payload)
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        await self._update_score_if_needed(payload, negated=True)
+        if payload.guild_id == self.guild.id:
+            await self._add_or_remove_reaction_from_db(payload, remove=True)
 
     @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        if before.display_name == after.display_name:
+    async def on_user_update(self, before, after):
+        if str(before) == str(after):
             return
 
-        m_id = after.id
-        new_nick = after.display_name
+        user_id = after.id
+        new_name = str(after)
         conn = sqlite3.connect(self.bot.config.db_path)
         c = conn.cursor()
-
-        if not c.execute("SELECT * FROM Members WHERE ID = ?", (m_id, )):
-            c.execute("INSERT INTO Members VALUES (?, ?, ?)", (
-                m_id,
-                new_nick,
-                0,
-            ))
-            conn.commit()
-
-        else:
-            c.execute("UPDATE Members SET DisplayName = ? WHERE ID = ?", (
-                new_nick,
-                m_id,
-            ))
-            conn.commit()
-
+        c.execute('INSERT OR REPLACE INTO Members VALUES (?,?)',
+                  (user_id, new_name))
+        conn.commit()
         conn.close()
 
     @commands.command()
-    async def ranking(self, ctx):
+    async def score(self, ctx, str1: str = None, *, str2: str = None):
+        """Display your score for a given emoji. If no arguments are given,
+           displays your total upmartlet/downmartlet score.
+           Arguments: An emoji (optional) and a mention (optional), in any order
         """
-        Upmartlet Rankings! :^)
-        """
-        await ctx.trigger_typing()
+        mentions = ctx.message.mentions
+        member = ctx.message.author
+        emoji = None
 
-        conn = sqlite3.connect(self.bot.config.db_path)
-        c = conn.cursor()
-        c.execute("SELECT * FROM Members ORDER BY Score DESC;")
-        members = c.fetchall()
+        if mentions:
+            member = mentions[0]
+            if str1 and str1.replace("!", "") != mentions[0].mention.replace(
+                    "!", ""):
+                emoji = str1
 
-        if not members:
-            await ctx.send(
-                "Ranking is not yet available for this server, please "
-                "upvote/downvote moar.")
-            return
+            if str2 and str2.replace("!", "") != mentions[0].mention.replace(
+                    "!", ""):
+                emoji = str2
+        elif str1:
+            emoji = str1
 
-        table = []
-        table_list = []
-        counter = 1
+        if emoji:
+            try:
+                await ctx.message.remove_reaction(emoji, self.bot.user)
+            except discord.errors.HTTPException:
+                await ctx.send("Invalid emoji or username")
+                return
 
-        for (ID, DisplayName, Upmartlet) in members:
-            table.append((counter, DisplayName, Upmartlet))
-            if counter % 7 == 0 or counter == len(members):
-                table_list.append(
-                    tabulate(table[:counter],
-                             headers=["Rank", "Name", "Score"],
-                             tablefmt="fancy_grid"))
-                del table[:]
-            counter += 1
-
-        p = Pages(ctx,
-                  item_list=table_list,
-                  title="Upmartlet ranking",
-                  display_option=(0, 1),
-                  editable_content=False)
-
-        await p.paginate()
-
-    @commands.command()
-    async def score(self, ctx, member: discord.Member = None):
-        """Displays member's score in upmartlet rankings."""
-
-        member = member if member else ctx.message.author
         m_id = member.id
         nick = member.display_name
 
         conn = sqlite3.connect(self.bot.config.db_path)
         c = conn.cursor()
 
-        c.execute("SELECT Score FROM Members WHERE ID = ?", (m_id, ))
-        score = c.fetchone()
+        if not emoji:
+            emoji = "total"
+            c.execute(
+                "SELECT count(ReacteeID) FROM Reactions WHERE ReacteeID = ? AND ReactionName = ? AND ReacterID != ReacteeID",
+                (m_id, str(self.UPMARTLET)))
+            upmartlets = c.fetchone()[0]
+            c.execute(
+                "SELECT count(ReacteeID) FROM Reactions WHERE ReacteeID = ? AND ReactionName = ? AND ReacterID != ReacteeID",
+                (m_id, str(self.DOWNMARTLET)))
+            downmartlets = c.fetchone()[0]
+            react_count = upmartlets - downmartlets
+        else:
+            c.execute(
+                "SELECT count(ReacteeID) FROM Reactions WHERE ReacteeID = ? AND ReactionName = ? AND ReacterID != ReacteeID",
+                (m_id, emoji))
+            react_count = c.fetchone()[0]
 
-        if not score:
-            score = (0, )
-            c.execute("INSERT INTO Members VALUES (?, ?, ?)", (m_id, nick, 0))
-            conn.commit()
+        await ctx.send("{}'s {} score is {}.".format(nick, emoji, react_count))
 
-        await ctx.send("{} score is {}.".format(nick, score[0]))
+        conn.close()
 
+    @commands.command()
+    async def ranking(self, ctx, emoji: str = None):
+        """Get the ranking for a given emoji. If no arguments are given,
+           displays your total upmartlet/downmartlet score.
+           Arguments: An emoji (optional)
+        """
+        conn = sqlite3.connect(self.bot.config.db_path)
+        c = conn.cursor()
+
+        if not emoji:
+            c.execute(
+                "SELECT M.Name, count(*) "
+                "FROM Reactions AS R, Members as M "
+                "WHERE R.ReactionName = ? "
+                "AND R.ReacterID != R.ReacteeID "
+                "AND R.ReacteeID = M.ID "
+                "GROUP BY R.ReacteeID "
+                "ORDER BY count(*) DESC", (str(self.UPMARTLET), ))
+            upmartlet_dict = dict(c.fetchall())
+            c.execute(
+                "SELECT M.Name, count(*) "
+                "FROM Reactions AS R, Members as M "
+                "WHERE R.ReactionName = ? "
+                "AND R.ReacterID != R.ReacteeID "
+                "AND R.ReacteeID = M.ID "
+                "GROUP BY R.ReacteeID "
+                "ORDER BY count(*) DESC", (str(self.DOWNMARTLET), ))
+            downmartlet_dict = dict(c.fetchall())
+            total_score_dict = {
+                key: upmartlet_dict.get(key, 0) - downmartlet_dict.get(key, 0)
+                for key in list(upmartlet_dict.keys()) +
+                list(downmartlet_dict.keys())
+            }
+            sorted_score_dict = dict(Counter(total_score_dict).most_common())
+
+            names = list(
+                map(lambda x, y: f"{y}. {x}", sorted_score_dict.keys(),
+                    range(1,
+                          len(sorted_score_dict) + 1)))
+            values = list(sorted_score_dict.values())
+            emoji = "Total"
+        else:
+            try:
+                await ctx.message.remove_reaction(emoji, self.bot.user)
+            except discord.errors.HTTPException:
+                await ctx.send("Invalid emoji")
+                return
+            c.execute(
+                "SELECT M.Name, count(*) "
+                "FROM Reactions AS R, Members as M "
+                "WHERE R.ReactionName = ? "
+                "AND R.ReacterID != R.ReacteeID "
+                "AND R.ReacteeID = M.ID "
+                "GROUP BY R.ReacteeID "
+                "ORDER BY count(*) DESC", (emoji, ))
+            counts = list(zip(*c.fetchall()))
+            if len(counts) == 0:
+                await ctx.send(embed=discord.Embed(
+                    title="This reaction was never used on this server."))
+                return
+            names = list(
+                map(lambda x, y: f"{y}. {x}", counts[0],
+                    range(1,
+                          len(counts[0]) + 1)))
+            values = counts[1]
+
+        paginator_dict = {"names": names, "values": values}
+        p = Pages(ctx,
+                  item_list=paginator_dict,
+                  title="{} score ranking".format(emoji),
+                  display_option=(2, 9),
+                  editable_content=False)
+
+        await p.paginate()
         conn.close()
 
 
