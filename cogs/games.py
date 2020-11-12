59124697cd4835da386d13f744dc73029bf27e09
namemcguffin@not-a-tower <52169277+namemcguffin@users.noreply.github.com>
2020-11-12 17:01:21 -0500

    Changed hangman docstring to be more accurate.

--- cogs/games.py
+++ cogs/games.py
@@ -47,7 +47,7 @@ class Games(commands.Cog):
     @commands.command(aliases=["hg"])
     async def hangman(self, ctx, command: str = None):
         """Play a nice game of hangman with internet strangers!
-        Guesses must be single letters (game is non case sensitive)
+        Guesses must be single letters (guesses must be lower case letters to be valid)
         Get all categories by typing "?hangman help"
         """
         if command == "help":
