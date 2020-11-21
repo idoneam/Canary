19cef8e8be29263f3ff2778b3897234c4db96af1
Kyle Rubenok <kyle@rubenok.ca>
2020-11-21 09:12:07 -0800

    revert to old "test" case

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+poetry run python3 Main.py &
+
+sleep 10
+
+PID=$!
+
+if ps -p $PID> /dev/null ; then
+	echo "found"
+	kill $PID
+	exit 0 
+else
+	echo "none found"
+	kill $PID
+	exit 1
+fi
