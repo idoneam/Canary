c93fef173678e232dd00c4a4f71eb53ad3ece2b9
David Lougheed <david.lougheed@gmail.com>
2020-11-28 12:23:38 -0500

    Put emoji strings into their own file

---
+++ cogs/utils/emojis.py
@@ -0,0 +1,48 @@
+# Copyright (C) idoneam (2016-2020)
+#
+# This file is part of Canary
+#
+# Canary is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canary is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Canary. If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = ["EMOJI", "NUMBERS"]
+
+EMOJI = {
+    "new": "🆕",
+    "mag": "🔍",
+    "pencil": "📝",
+    "stop_button": "⏹",
+    "ok": "🆗",
+    "white_check_mark": "✅",
+    "x": "❌",
+    "put_litter_in_its_place": "🚮",
+    "rewind": "⏪",
+    "arrow_backward": "◀",
+    "arrow_forward": "▶",
+    "fast_forward": "⏩",
+    "zero": "0️⃣",
+    "one": "1️⃣",
+    "two": "2️⃣",
+    "three": "3️⃣",
+    "four": "4️⃣",
+    "five": "5️⃣",
+}
+
+NUMBERS = (
+    EMOJI['zero'],
+    EMOJI['one'],
+    EMOJI['two'],
+    EMOJI['three'],
+    EMOJI['four'],
+    EMOJI['five'],
+)
