31fc43958cb7714ed19a8abec4644cf14006c3bf
namemcguffin@not-a-tower <52169277+namemcguffin@users.noreply.github.com>
2020-12-27 12:31:58 -0500

    Added movie name list.

--- cogs/utils/hangman.py
+++ cogs/utils/hangman.py
@@ -75,8 +75,12 @@ def mk_country_list() -> List[Tuple[str, str]]:
         if len(curr_table) != 4 or i in (227, 228, 229):
             continue
         country_name_entry = curr_table[0].find("a")
+        country_name = str(country_name_entry.contents[0]).lower()
+        if "," in country_name:
+            comma_index = country_name.index(",")
+            country_name = f"{country_name[comma_index+2:]} {country_name[:comma_index]}"
         country_list.append(
-            (str(country_name_entry.contents[0]).lower(),
+            (country_name,
              "https:" + BeautifulSoup(
                  requests.get(
                      f"https://en.wikipedia.org{country_name_entry['href']}").
@@ -113,15 +117,35 @@ def mk_element_list() -> List[Tuple[str, str]]:
 
 
 def mk_movie_list() -> List[Tuple[str, str]]:
-    return None
+    kino_elem_soup = BeautifulSoup(
+        requests.get(
+            "https://en.wikipedia.org/wiki/List_of_years_in_film").content,
+        "html.parser").find_all("i")
+    kino_list: List[Tuple[str, str]] = []
+    for i in range(195, len(kino_elem_soup)):
+        kino_elem = kino_elem_soup[i].find("a")
+        if kino_elem:
+            try:
+                kino_img = "https:" + BeautifulSoup(
+                        requests.get(f"https://en.wikipedia.org{kino_elem['href']}"
+                                    ).content, "html.parser").find(
+                                        "table", {
+                                            "class": "infobox"
+                                        }).find("a", {
+                                            "class": "image"
+                                        }).find("img")["src"]
+            except AttributeError:
+                continue
+            kino_list.append((str(kino_elem.contents[0]).lower(), kino_img))
+    return kino_list
 
 
 def mk_hangman_dict(file_name) -> Dict[str, List[Tuple[str, str]]]:
     with open(f"pickles/premade/{file_name}.obj", "wb") as dump_file:
         pickle.dump(
             {
-                "animal": (mk_animal_list(), "animals"),
-                "myth": (mk_mythical_list(), "mythical creatures"),
+                #"animal": (mk_animal_list(), "animals"),
+                #"myth": (mk_mythical_list(), "mythical creatures"),
                 "country": (mk_country_list(), "country names"),
                 "element": (mk_element_list(), "elements"),
                 "movie": (mk_movie_list(), "movies")
