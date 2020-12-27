25aa8031ca66e67a05ca380b5d5989d72294b2a2
namemcguffin@not-a-tower <52169277+namemcguffin@users.noreply.github.com>
2020-12-27 16:50:29 -0500

    Fixed max_len not being used

--- cogs/utils/hangman.py
+++ cogs/utils/hangman.py
@@ -146,7 +146,7 @@ def mk_hangman_str(first, num_mistakes, last) -> str:
 
 def append_and_slice(inp_list, elem, max_len=3):
     inp_list.append(elem)
-    inp_list = inp_list[-3:]
+    inp_list = inp_list[-max_len:]
 
 
 def mk_hangman_dict(file_name) -> Dict[str, List[Tuple[str, str]]]:
