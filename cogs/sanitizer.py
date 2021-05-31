adf345d1d1e394229708a8d0432b43bc1ca8a602
namemcguffin@not-a-tower <52169277+namemcguffin@users.noreply.github.com>
2021-05-31 18:00:25 -0400

    you assumed poorly friend

--- cogs/sanitizer.py
+++ cogs/sanitizer.py
@@ -21,11 +21,20 @@ import re
 import requests
 
 TIKTOK_SHORTLINK = re.compile(r"https?:\/\/vm\.tiktok\.com\/[A-Za-z0-9]*")
-TIKTOK_MOBILE = re.compile(r"(https?:\/\/m\.tiktok\.com\/v\/[0-9]*)\.html\?[A-Za-z0-9_&=%\.\?-]*")
-TIKTOK_DESKTOP = re.compile(r"(https?:\/\/www\.tiktok\.com\/@[A-Za-z0-9_\.]*\/video\/[0-9]*)\?[A-Za-z0-9_&=%\.\?-]*")
+TIKTOK_MOBILE = re.compile(
+    r"(https?:\/\/m\.tiktok\.com\/v\/[0-9]*)\.html\?[A-Za-z0-9_&=%\.\?-]*")
+TIKTOK_DESKTOP = re.compile(
+    r"(https?:\/\/www\.tiktok\.com\/@[A-Za-z0-9_\.]*\/video\/[0-9]*)\?[A-Za-z0-9_&=%\.\?-]*"
+)
+
 
 def unroll_tiktok(link):
-    return requests.head(link, headers={"User-Agent": "Mozilla/5.0 (X11)"}, allow_redirects=True).url
+    return requests.head(link,
+                         headers={
+                             "User-Agent": "Mozilla/5.0 (X11)"
+                         },
+                         allow_redirects=True).url
+
 
 class Sanitizer(commands.Cog):
     def __init__(self, bot):
@@ -33,7 +42,9 @@ class Sanitizer(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def tiktok_link_sanitizer(self, msg):
-        if (not isinstance(msg.author, discord.Member)) or utils.get(msg.author.roles, name=self.bot.config.sanitation["tt_optout"]):
+        if (not isinstance(msg.author, discord.Member)) or utils.get(
+                msg.author.roles,
+                name=self.bot.config.sanitation["tt_optout"]):
             return
         replace: bool = False
         msg_txt: str = str(msg.content)
@@ -52,16 +63,21 @@ class Sanitizer(commands.Cog):
         if replace:
             await msg.delete()
             await msg.channel.send(f"from: `{msg.author}`\n>>> {msg_txt}")
-            if not utils.get(msg.author.roles, name=self.bot.config.sanitation["tt_silent"]):
-                dm_channel = msg.author.dm_channel or await msg.author.create_dm()
-                await dm_channel.send(f"WARNING: a message you sent contained a tiktok link"
-                                       " that could potentially contain sensitive information."
-                                       "\nas such, it has been deleted and a sanitized version of"
-                                       " the message was resent.\nto opt out of this feature, feel"
-                                      f" free to request the `{self.bot.config.sanitation['tt_optout']}`"
-                                       "role.\nto still have this feature enabled but to no longer receive"
-                                      f" these message, feel free to request the `{self.bot.config.sanitation['tt_silent']}`"
-                                      f" role.\nhere is the message in question:\n>>> {msg.content}")
+            if not utils.get(msg.author.roles,
+                             name=self.bot.config.sanitation["tt_silent"]):
+                dm_channel = msg.author.dm_channel or await msg.author.create_dm(
+                )
+                await dm_channel.send(
+                    f"WARNING: a message you sent contained a tiktok link"
+                    " that could potentially contain sensitive information."
+                    "\nas such, it has been deleted and a sanitized version of"
+                    " the message was resent.\nto opt out of this feature, feel"
+                    f" free to request the `{self.bot.config.sanitation['tt_optout']}`"
+                    "role.\nto still have this feature enabled but to no longer receive"
+                    f" these message, feel free to request the `{self.bot.config.sanitation['tt_silent']}`"
+                    f" role.\nhere is the message in question:\n>>> {msg.content}"
+                )
+
 
 def setup(bot):
     bot.add_cog(Sanitizer(bot))
