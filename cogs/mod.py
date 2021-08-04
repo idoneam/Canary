60100cbe591544838ae58d0a84402ea7707e50df
Valerian Clerc <valerian.clerc@gmail.com>
2021-08-04 19:08:55 -0400

    Added support for linking to channels by name

--- cogs/mod.py
+++ cogs/mod.py
@@ -57,12 +57,42 @@ class Mod(commands.Cog):
     @commands.command()
     @is_moderator()
     async def update_rules(self, ctx, *args):
-        rules_file = requests.get(url = self.bot.config.rules_url).text
+        rules_file = requests.get(url = self.bot.config.rules_url).text  # TODO: use async "GET" from our utils
         channel = discord.utils.get(ctx.guild.channels, name=self.bot.config.rules_channel)
         deleted = await channel.purge(limit=10000)
         rules = rules_file.split("#NEWMSG")
         for rule in rules:
+            # parse any channel references in the messages (i.e. "#bots", "#verified_general", etc)
+            flag = "#CHANNEL"
+            rule = self.parseAndReplace(rule, flag, self.lookupChannel, ctx)
+
             await ctx.send(content=rule, embed=None)
+
+
+    # Handler function for responding to #CHANNEL directive
+    def lookupChannel(self, ctx, channelName: str):
+        channel = discord.utils.get(ctx.guild.channels, name=channelName)
+        if hasattr(channel, "id"):
+            return f"<#{channel.id}>"
+        else:
+            return f"#{channelName}"
+
+
+    # Overengineered helper function that replaces every instance of "flag" with "handler(flagContents)"
+    # i.e. goes through "mainStr" and wherever there's an instance of flag, it takes the contents
+    # of flag and runs the handler on them, and then places the output at the flags location in the string
+    #
+    # ex: mainStr = "1234#MYFLAG(AAA)5678#MYFLAG(BBBB)91011", flag = "#MYFLAG", handler = toLower function
+    # result: 1234aaa5678bbbb91011
+    def parseAndReplace(self, mainStr: str, flag: str, handler, ctx):
+        while flag in mainStr:
+            channel_i = mainStr.find(flag, 0, len(mainStr))
+            end_i = mainStr[channel_i:].find(")") + channel_i
+            contents = mainStr[channel_i + len(flag) + 1:end_i]
+            contents = handler(ctx, contents)
+            mainStr = mainStr[:channel_i] + contents + mainStr[end_i + 1:]
+        return mainStr
+
 
 def setup(bot):
     bot.add_cog(Mod(bot))
