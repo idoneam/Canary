99ab5e7f8afc646bd8b0dd6480847a68fac5f311
Mathieu B <47489943+le-potate@users.noreply.github.com>
2022-08-31 22:13:11 -0400

    minor fix: remove double datetime import

--- cogs/mod.py
+++ cogs/mod.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Canary. If not, see <https://www.gnu.org/licenses/>.
 
-import datetime
 import discord
 import sqlite3
 import random
